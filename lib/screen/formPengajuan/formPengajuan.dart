74592ffe50be261b97cbd6a771f1770a85f0665e
BENTOTV <agusjuniartha16@gmail.com>
2023-11-16 15:15:23 +0700

    menambahkan update terbaru

--- lib/screen/formPengajuan/formPengajuan.dart
+++ lib/screen/formPengajuan/formPengajuan.dart
@@ -4,7 +4,6 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hki_app/model/modelAdj.dart';
 import 'package:hki_app/screen/mainPage/mainPage.dart';
 import 'package:hki_app/screen/riwayatPengajuan/riwayatPengajuan.dart';
@@ -22,64 +21,68 @@ class FormPengajuan extends StatefulWidget {
 }
 
 class _FormPengajuanState extends State<FormPengajuan> {
-   FilePickerResult? result;
+  FilePickerResult? result;
   late File display;
   late String adjustment;
   late String nama;
   late String jabatan;
-  
+
   @override
   Widget build(BuildContext context) {
-     final adjProvider= Provider.of<adj_logic.AdjLogic>(context,listen: false);
-    var formpengajuankey =  GlobalKey<FormState>();
+    final adjProvider = Provider.of<adj_logic.AdjLogic>(context, listen: false);
+    var formpengajuankey = GlobalKey<FormState>();
     final _NamaController = TextEditingController();
-  final _JabatanController = TextEditingController();
-  final _AdjController = TextEditingController();
-  DateTime _dueDate = DateTime.now();
-  @override
-  void dispose() {
-    _JabatanController.dispose();
-    _NamaController.dispose();
-    _AdjController.dispose();
-    super.dispose();
-  }
-  @override
-  void initState() {
-    
-    super.initState();
-  }
-   return Scaffold(
-      appBar: AppBar(backgroundColor: kPrimaryColor,),
+    final _JabatanController = TextEditingController();
+    final _AdjController = TextEditingController();
+    DateTime _dueDate = DateTime.now();
+    @override
+    void dispose() {
+      _JabatanController.dispose();
+      _NamaController.dispose();
+      _AdjController.dispose();
+      super.dispose();
+    }
+
+    @override
+    void initState() {
+      super.initState();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+      ),
       body: SingleChildScrollView(
         child: Form(
           key: formpengajuankey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            
             children: <Widget>[
               Card(
-                margin: EdgeInsets.only(left: 50,right: 50),
-                elevation: 8,              
+                margin: EdgeInsets.only(left: 50, right: 50),
+                elevation: 8,
                 child: Padding(
-                  padding: const EdgeInsets.only(right: 20,left: 20,top: 30,bottom: 30),
+                  padding: const EdgeInsets.only(
+                      right: 20, left: 20, top: 30, bottom: 30),
                   child: Column(
                     children: <Widget>[
-                     // Image.asset("assests/image/logoBhawana.png",width: 100,height: 100,),
-                      Text("Form Pengajuan",style: GoogleFonts.poppins(fontSize: 14),),
+                      // Image.asset("assests/image/logoBhawana.png",width: 100,height: 100,),
+                      Text(
+                        "Form Pengajuan",
+                        style: TextStyle(fontSize: 14),
+                      ),
                       TextFormField(
                         controller: _NamaController,
                         keyboardType: TextInputType.text,
-                        style: GoogleFonts.poppins(fontSize: 14),
+                        style: TextStyle(fontSize: 14),
                         decoration: InputDecoration(
-                         // hintText: "Masukan Username",
-                          hintStyle: GoogleFonts.poppins(fontSize: 14),
-                          labelText: "Nama Pangaju"
-                        ),
+                            // hintText: "Masukan Username",
+                            hintStyle: TextStyle(fontSize: 14),
+                            labelText: "Nama Pangaju"),
                         validator: (value) {
                           if (value!.isEmpty) {
                             return "Nama tidak boleh kosong";
-                          }
-                          else{
+                          } else {
                             return null;
                           }
                         },
@@ -87,87 +90,94 @@ class _FormPengajuanState extends State<FormPengajuan> {
                       TextFormField(
                         controller: _JabatanController,
                         keyboardType: TextInputType.text,
-                        style: GoogleFonts.poppins(fontSize: 14),
+                        style: TextStyle(fontSize: 14),
                         decoration: InputDecoration(
-                          //hintText: "Masukan Password",
-                          hintStyle: GoogleFonts.poppins(fontSize: 14),
-                          labelText: "Jabatan Pengaju"
-                        ),
+                            //hintText: "Masukan Password",
+                            hintStyle: TextStyle(fontSize: 14),
+                            labelText: "Jabatan Pengaju"),
                         validator: (value) {
                           if (value!.isEmpty) {
                             return "Jabatan tidak boleh kosong";
-                          }
-                          else{
+                          } else {
                             return null;
                           }
                         },
                       ),
-                        TextFormField(
+                      TextFormField(
                         controller: _AdjController,
                         keyboardType: TextInputType.multiline,
-                        style: GoogleFonts.poppins(fontSize: 14),
+                        style: TextStyle(fontSize: 14),
                         maxLines: 4,
                         decoration: InputDecoration(
-                          //hintText: "Masukan Password",
-                          hintStyle: GoogleFonts.poppins(fontSize: 14),
-                          labelText: "Adjusment"
-                        ),
+                            //hintText: "Masukan Password",
+                            hintStyle: TextStyle(fontSize: 14),
+                            labelText: "Adjusment"),
                         validator: (value) {
                           if (value!.isEmpty) {
                             return "Adjusment tidak boleh kosong";
-                          }
-                          else{
+                          } else {
                             return null;
                           }
                         },
                       ),
-                      SizedBox(height: 20,),
-                       const Text("Pick Files"),
-                              const SizedBox(height: 10,),
-                              Center(
-                                child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: kPrimaryColor
-                                  ),
-                                  child: const Text("Pick and Open Files"),
-                                  onPressed: () {
-                                    _pickFile();
-                                  },
-                                ),
-                              ),
-                      SizedBox(height: 20,),
-                    //  SizedBox(child: DropDownItem(),),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      const Text("Pick Files"),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Center(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: kPrimaryColor),
+                          child: const Text("Pick and Open Files"),
+                          onPressed: () {
+                            _pickFile();
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      //  SizedBox(child: DropDownItem(),),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: <Widget>[
                           TextButton(
-                            style: TextButton.styleFrom(
-                              backgroundColor: Colors.blue,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
-                            ),
-                            onPressed: (){                      
-                              final isValidForm = formpengajuankey.currentState!.validate();
-                              String usernmer = _NamaController.text;  
-                              // adjustment = _AdjController.value as String;
-                              // jabatan = _JabatanController.value as String;
-                              // nama = _NamaController.value as String;
-                              print(usernmer);
-                              if(isValidForm){
-                                adjProvider.add(
-                                  Adj(
-                                    adjustment: _AdjController.text.toString(),
-                                     display: display,
-                                      jabatan: _JabatanController.text.toString(), 
-                                      nama: _NamaController.text.toString(), 
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20))),
+                              onPressed: () {
+                                final isValidForm =
+                                    formpengajuankey.currentState!.validate();
+                                String usernmer = _NamaController.text;
+                                // adjustment = _AdjController.value as String;
+                                // jabatan = _JabatanController.value as String;
+                                // nama = _NamaController.value as String;
+                                print(usernmer);
+                                if (isValidForm) {
+                                  adjProvider.add(Adj(
+                                      adjustment:
+                                          _AdjController.text.toString(),
+                                      display: display,
+                                      jabatan:
+                                          _JabatanController.text.toString(),
+                                      nama: _NamaController.text.toString(),
                                       id: 1,
-                                      tgl: _dueDate)
-                                );
-                                Navigator.push(context, MaterialPageRoute(builder: (context)=>MainPage()));
-                              }                         
-                            }, 
-                            child: const Text("Submit",style: TextStyle(
-                              fontSize: 14,color: Colors.white
-                            ),)),
+                                      tgl: _dueDate));
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MainPage()));
+                                }
+                              },
+                              child: const Text(
+                                "Submit",
+                                style: TextStyle(
+                                    fontSize: 14, color: Colors.white),
+                              )),
                         ],
                       )
                     ],
@@ -180,28 +190,21 @@ class _FormPengajuanState extends State<FormPengajuan> {
       ),
     );
   }
-   void _pickFile() async{
-  try {
-    final result = await FilePicker.platform.pickFiles(
-  type: FileType.any   
-  );
-  final file = result!.files.first;
-  setState(() {
-    display = File(file.path.toString());
-  });
-  
 
+  void _pickFile() async {
+    try {
+      final result = await FilePicker.platform.pickFiles(type: FileType.any);
+      final file = result!.files.first;
+      setState(() {
+        display = File(file.path.toString());
+      });
+    } catch (e) {}
 
-  } catch (e) {
-    
+    // final file = result.files.first;
+    // _openFile(file);
   }
-  
-  
-  // final file = result.files.first;
-  // _openFile(file);
-}
-void _openFile(PlatformFile file){
-OpenFile.open(file.path);
-}
-}
 
+  void _openFile(PlatformFile file) {
+    OpenFile.open(file.path);
+  }
+}
